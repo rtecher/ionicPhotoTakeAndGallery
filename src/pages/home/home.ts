77bd046812564aa9c8dff856e8db1339b32f3372
rtecher <abdurrehim.sen@bil.omu.edu.tr>
2018-08-06 18:09:55 +0300

    initial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,56 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public base64Image: string;
 
+  constructor(public navCtrl: NavController,
+  	private camera: Camera,
+  	private toastCtrl: ToastController) {
+
+  }
+
+  takePicture(){
+    console.log("coming here");
+
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 450,
+      targetHeight: 450,
+      correctOrientation: true,
+      saveToPhotoAlbum: false
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+      },
+      err => {
+        console.log(err);
+        let toast = this.toastCtrl.create({
+		    message: err,
+		    duration: 3000,
+		    position: 'top'
+		  });
+        toast.present();
+      }
+    );
+  }
+
+  openGallery() {
+  	const options: CameraOptions = {
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      saveToPhotoAlbum: false
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
